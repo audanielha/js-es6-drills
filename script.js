4dc0510ee5b80370a8a9ef0bd0b9c6b31954d4be
Daniel Ha <audanielha@gmail.com>
2024-12-02 22:52:08 +1300

    finishing remaining drills

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-function favouriteMovie(name ="Tommy Wiseau", movie = "The Room"){
-    console.log(`My name is ${name} and my favourite move is ${movie}`)
+// function favouriteMovie(name ="Tommy Wiseau", movie = "The Room"){
+//     console.log(`My name is ${name} and my favourite move is ${movie}`)
+// }
+
+// favouriteMovie()
+
+// let favouriteMovie = movie => console.log(`My favourite movie is ${movie}`)
+
+// favouriteMovie()
+
+// let getFirstName = (name) =>{
+//     let firstName = name.split(" ")
+//     return firstName[0]
+// }
+
+// let getFirstNameConcise = name => name.split(" ")[0]
+
+
+// let arr = ["Paul", "Birmingham", "Kimchi"];
+
+// let foodPlace = (name, location, favFood) => {
+//     // let [a, b, c] = [name, location, favFood];
+//     console.log(`${name}, ${location}, ${favFood}`)
+// }
+
+// foodPlace(...arr)
+
+let myName = "Daniel";
+
+let example = (x) =>{
+let speard =[...x]
+for(let i = 0; i < speard.length; i++){
+    console.log(speard[i])
+}
 }
 
-favouriteMovie()
+console.log(example(myName))
